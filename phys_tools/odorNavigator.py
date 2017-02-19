63b79fad52425d69b0414584fd02400c067e87ab
Chris Wilson <cdw291@nyu.edu>
2017-02-19 12:41:24 -0500

    Initial commit of odorNavigator. Refactor SpatialMapWidget to generic PsthViewWidget that can be reused between guis with different stimuli. Make gui initial sizes more reasonable

---
+++ phys_tools/odorNavigator.py
@@ -0,0 +1,16 @@
+from phys_tools.views import main_views, odor_views
+from phys_tools.models import OdorSession
+from PyQt5.QtWidgets import QApplication
+
+
+def main():
+    import sys
+    app = QApplication(sys.argv)
+    w = main_views.MainWindow(OdorSession, odor_views.OdorSessionWidget)
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
